e62719615e51216cf1d7730f3b39efa88ae1ba4a
David Chen <dchen5085@gmail.com>
2016-03-27 20:23:05 -0400

    add setting to draw polygons

--- dclib/core/src/main/java/dclib/epf/util/EntityDrawer.java
+++ dclib/core/src/main/java/dclib/epf/util/EntityDrawer.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 import dclib.epf.Entity;
 import dclib.epf.EntityManager;
@@ -14,33 +16,62 @@ import dclib.epf.parts.TransformPart;
 
 public final class EntityDrawer {
 
+	private boolean drawPolygons = false;
 	private final EntityManager entityManager;
 	private final PolygonSpriteBatch spriteBatch;
+	private final ShapeRenderer shapeRenderer = new ShapeRenderer();
 	private final Camera camera;
+	private final float pixelsPerUnit;
 	
-	public EntityDrawer(final EntityManager entityManager, final PolygonSpriteBatch spriteBatch, final Camera camera) {
+	public EntityDrawer(final EntityManager entityManager, final PolygonSpriteBatch spriteBatch, final Camera camera, 
+			final float pixelsPerUnit) {
 		this.entityManager = entityManager;
 		this.spriteBatch = spriteBatch;
 		this.camera = camera;
+		this.pixelsPerUnit = pixelsPerUnit;
 	}
 	
-	public void draw() {
+	public final void setPolygonDrawing(final boolean drawPolygons) {
+		this.drawPolygons = drawPolygons;
+	}
+	
+	public final void draw() {
+		List<Entity> entities = entityManager.getAll();
+		final EntityZComparator entityZComparator = new EntityZComparator();
+		Collections.sort(entities, entityZComparator);
+		drawSprites(entities);
+		if (drawPolygons) {
+			drawPolygons(entities);
+		}
+	}
+	
+	private void drawSprites(final List<Entity> entities) {
 		spriteBatch.setProjectionMatrix(camera.combined);
 		spriteBatch.begin();
-		final EntityZComparator entityZComparator = new EntityZComparator();
-		List<Entity> entities = entityManager.getAll();
-		Collections.sort(entities, entityZComparator);
-		draw(entities);
-		spriteBatch.end();
-	}
-	
-	private void draw(final List<Entity> entities) {
 		for (Entity entity : entities) {
 			if (entity.hasActive(DrawablePart.class)) {
 				DrawablePart drawablePart = entity.get(DrawablePart.class);
 				drawablePart.getSprite().draw(spriteBatch);
 			}
 		}
+		spriteBatch.end();
+	}
+	
+	private void drawPolygons(final List<Entity> entities) {
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeType.Line);
+		for (Entity entity : entities) {
+			if (entity.hasActive(TransformPart.class)) {
+				TransformPart transformPart = entity.get(TransformPart.class);
+				float[] transformedVertices = transformPart.getPolygon().getTransformedVertices();
+				float[] vertices = new float[transformedVertices.length];
+				for (int i = 0; i < transformedVertices.length; i++) {
+					vertices[i] = transformedVertices[i] * pixelsPerUnit;
+				}
+				shapeRenderer.polygon(vertices);
+			}
+		}
+		shapeRenderer.end();
 	}
 	
 	private static class EntityZComparator implements Comparator<Entity> {
