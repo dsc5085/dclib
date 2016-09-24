9e73e203ce9d9227d45ca1101f06dd7f3a9d526f
David Chen <dchen5085@gmail.com>
2016-09-23 22:29:44 -0400

    Update dependency versions

--- dclib/core/src/main/java/dclib/geometry/Point.java
+++ dclib/core/src/main/java/dclib/geometry/Point.java
@@ -4,18 +4,18 @@ public final class Point {
 
 	private final int x;
 	private final int y;
-	
+
 	public Point(final int x, final int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public final int x() {
 		return x;
 	}
-	
+
 	public final int y() {
 		return y;
 	}
-	
+
 }
