7692bb3b12eafee9c767af6b73204e79422d9a23
dave1 <dave1@LAPTOP-4I0UEUC1.home>
2016-11-06 10:36:18 -0500

    Utility method to set filter

--- dclib/core/src/main/java/dclib/physics/Box2dUtils.kt
+++ dclib/core/src/main/java/dclib/physics/Box2dUtils.kt
@@ -1,8 +1,7 @@
 package dclib.physics
 
 import com.badlogic.gdx.physics.box2d.Body
-import com.badlogic.gdx.physics.box2d.World
-import com.badlogic.gdx.utils.Array
+import com.badlogic.gdx.physics.box2d.Filter
 import dclib.epf.Entity
 import dclib.epf.parts.TransformPart
 
@@ -15,5 +14,14 @@ object Box2dUtils {
 	fun getBody(entity: Entity): Body? {
 		val transform = entity.tryGet(TransformPart::class.java).transform
 		return if (transform is Box2dTransform) transform.body else null
+	}
+
+	fun setFilter(body: Body, category: Short, mask: Short) {
+		val filter = Filter()
+		filter.categoryBits = category
+		filter.maskBits = mask
+		for (fixture in body.fixtureList) {
+			fixture.filterData = filter
+		}
 	}
 }
