12764c961234dcce35a673f78c32310e4646bf48
dsc5085 <dchen5085@gmail.com>
2016-11-07 22:03:27 -0500

    Moved dependencies to app-level gradle file.  Fixed PathUtils to have root at working directory

--- dclib/core/src/main/java/dclib/util/PathUtils.java
+++ dclib/core/src/main/java/dclib/util/PathUtils.java
@@ -1,18 +1,12 @@
 package dclib.util;
 
-import java.net.URL;
-
 public final class PathUtils {
 
 	private PathUtils() {
 	}
 	
 	public static final String internalToAbsolutePath(final String internalPath) {
-		URL url = PathUtils.class.getResource("/" + internalPath);
-		if (url == null) {
-			throw new IllegalArgumentException("File does not exist at " + internalPath);
-		}
-		return PathUtils.class.getResource("/" + internalPath).getPath();
+		return System.getProperty("user.dir") + "/" + internalPath;
 	}
 	
 }
