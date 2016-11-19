f1b3e0b342d333d1db0743fc2bac74a841562c19
dsc5085 <dchen5085@gmail.com>
2016-11-19 15:12:30 -0500

    Make vector2 assertion method more descriptive

--- dclib/core/src/main/java/test/dclib/geometry/VectorTestUtils.java
+++ dclib/core/src/main/java/test/dclib/geometry/VectorTestUtils.java
@@ -2,6 +2,7 @@ package test.dclib.geometry;
 
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import dclib.util.Maths;
 import org.junit.Assert;
 
 public class VectorTestUtils {
@@ -10,8 +11,10 @@ public class VectorTestUtils {
 	}
 
 	public static void assertEquals(final float expectedX, final float expectedY, final Vector2 vector) {
-		Assert.assertEquals(expectedX, vector.x, MathUtils.FLOAT_ROUNDING_ERROR);
-		Assert.assertEquals(expectedY, vector.y, MathUtils.FLOAT_ROUNDING_ERROR);
-	}
+        Vector2 expected = new Vector2(expectedX, expectedY);
+        Assert.assertTrue("Expected: " + expected + " Actual: " + vector,
+                Maths.distance(vector.x, expectedX) < MathUtils.FLOAT_ROUNDING_ERROR &&
+                        Maths.distance(vector.y, expectedY) < MathUtils.FLOAT_ROUNDING_ERROR);
+    }
 
 }
