e241f0b49914263d80d21332623974f5a47b4928
David Chen <dchen5085@gmail.com>
2017-05-21 19:54:33 -0400

    Updated screen manager and input

--- dclib/core/src/main/java/dclib/system/Input.java
+++ dclib/core/src/main/java/dclib/system/Input.java
@@ -1,24 +1,20 @@
 package dclib.system;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Input {
 
 	private static final InputMultiplexer multiplexer = new InputMultiplexer();
 
-	private final Set<InputProcessor> processors = new HashSet<InputProcessor>();
+	private final List<InputProcessor> processors = new ArrayList<InputProcessor>();
 
 	public static final boolean contains(final InputProcessor processor) {
 		return multiplexer.getProcessors().contains(processor, true);
-	}
-
-	public static final InputProcessor getProcessor(final int index) {
-		return multiplexer.getProcessors().get(index);
 	}
 
 	public final void add(final InputProcessor processor) {
@@ -26,12 +22,17 @@ public final class Input {
 	}
 
 	public final void add(final InputProcessor processor, final int index) {
-		processors.add(processor);
-		multiplexer.addProcessor(index, processor);
+		processors.add(index, processor);
+	}
+
+	public final void enable() {
+		for (InputProcessor processor : processors) {
+			multiplexer.addProcessor(processor);
+		}
 		Gdx.input.setInputProcessor(multiplexer);
 	}
 
-	public final void dispose() {
+	public final void disable() {
 		for (InputProcessor processor : processors) {
 			multiplexer.removeProcessor(processor);
 		}
--- dclib/core/src/main/java/dclib/system/ScreenManager.kt
+++ dclib/core/src/main/java/dclib/system/ScreenManager.kt
@@ -4,34 +4,43 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 
 class ScreenManager {
-    private val screens = mutableListOf<Screen>()
+    private val screenStates = mutableListOf<ScreenState>()
 
     val currentScreen: Screen
         get() {
-            if (screens.isEmpty()) {
+            if (screenStates.isEmpty()) {
                 throw UnsupportedOperationException("There are no screens")
             }
-            return screens.first()
+            return screenStates.first().screen
         }
 
     operator fun contains(screen: Screen): Boolean {
-        return screens.contains(screen)
+        return getScreens().contains(screen)
     }
 
-    fun add(screen: Screen) {
-        screens.add(screen)
+    fun enable(screen: Screen) {
+        screenStates.first { it.screen === screen }.isEnabled = true
         screen.show()
     }
 
-    fun addHidden(screen: Screen) {
-        screens.add(screen)
+    fun disable(screen: Screen) {
+        screenStates.first { it.screen === screen }.isEnabled = false
         screen.hide()
+    }
+
+    fun add(screen: Screen, isEnabled: Boolean = true) {
+        screenStates.add(ScreenState(screen, isEnabled))
+        if (isEnabled) {
+            enable(screen)
+        } else {
+            disable(screen)
+        }
     }
 
     fun remove(screen: Screen) {
         screen.hide()
         screen.dispose()
-        screens.remove(screen)
+        screenStates.removeAll { it.screen === screen }
     }
 
     fun swap(newScreen: Screen) {
@@ -42,24 +51,27 @@ class ScreenManager {
     }
 
     fun render() {
-        for (screen in getScreens()) {
+        val enabledScreens = screenStates.filter { it.isEnabled }.map { it.screen }
+        for (screen in enabledScreens) {
             screen.render(Gdx.graphics.deltaTime)
         }
     }
 
     fun resize(width: Int, height: Int) {
-        for (screen in screens) {
+        for (screen in getScreens()) {
             screen.resize(width, height)
         }
     }
 
     fun dispose() {
-        for (screen in screens) {
-            screen.dispose()
+        for (screen in getScreens()) {
+            remove(screen)
         }
     }
 
     private fun getScreens(): List<Screen> {
-        return screens.toList()
+        return screenStates.map { it.screen }.toList()
     }
+
+    private inner class ScreenState(val screen: Screen, var isEnabled: Boolean)
 }
