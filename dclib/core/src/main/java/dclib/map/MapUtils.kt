a298e03cc16a0b19fb3816147894a97db930347b
David Chen <dchen5085@gmail.com>
2017-12-20 21:32:10 -0500

    Utility method updates

--- dclib/core/src/main/java/dclib/map/MapUtils.kt
+++ dclib/core/src/main/java/dclib/map/MapUtils.kt
@@ -2,6 +2,7 @@ package dclib.map
 
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
+import dclib.geometry.Point
 
 object MapUtils {
     val BACKGROUND_INDEX = 0
@@ -10,5 +11,18 @@ object MapUtils {
     fun getPixelsPerUnit(map: TiledMap): Float {
         val layer = map.layers.first() as TiledMapTileLayer
         return Math.max(layer.tileWidth, layer.tileHeight)
+    }
+
+    fun getCells(layer: TiledMapTileLayer): List<Cell> {
+        val cells = mutableListOf<Cell>()
+        for (x in 0 until layer.width - 1) {
+            for (y in 0 until layer.height - 1) {
+                val cell = layer.getCell(x, y)
+                if (cell != null) {
+                    cells.add(Cell(cell, Point(x, y)))
+                }
+            }
+        }
+        return cells
     }
 }
