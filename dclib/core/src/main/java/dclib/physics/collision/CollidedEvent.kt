b1bc637c8674feddcc9f9b06e0ac5d44f7d8f300
dsc5085 <dchen5085@gmail.com>
2018-01-13 10:15:46 -0500

    Sped up collision checker. No longer stores duplicate collisions

--- dclib/core/src/main/java/dclib/physics/collision/CollidedEvent.kt
+++ dclib/core/src/main/java/dclib/physics/collision/CollidedEvent.kt
@@ -1,7 +1,3 @@
 package dclib.physics.collision
 
-import dclib.epf.Entity
-
-data class CollidedEvent(val source: Entity, val target: Entity) {
-    val collisions = mutableListOf<Collision>()
-}
+data class CollidedEvent(val collision: Collision)
--- dclib/core/src/main/java/dclib/physics/collision/Collision.kt
+++ dclib/core/src/main/java/dclib/physics/collision/Collision.kt
@@ -1,10 +1,18 @@
 package dclib.physics.collision
 
 import com.badlogic.gdx.math.Vector2
+import com.google.common.base.Objects
 
-data class Collision(
+class Collision(
         val source: Contacter,
         val target: Contacter,
         val isTouching: Boolean,
-        val manifold: List<Vector2>
-)
+        val manifold: List<Vector2>) {
+    override fun equals(other: Any?): Boolean {
+        return other is Collision && source == other.source && target == other.target
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hashCode(source, target)
+    }
+}
